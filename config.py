10d65aa880f7806844789fd3a1390db201583b10
Izamu165 <koliviraj541@gmail.com>
2025-05-23 09:39:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/OGNOBITA12/Toptenmusic",
+    "https://github.com/Izamu165/Tizamumusic-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
