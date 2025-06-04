74c3eddcd6f8219e1174b76d94128915174b2dea
nishamusicbot1 <sachin34u7@gmail.com>
2025-06-04 15:21:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Izamu165/Tizamumusic-",
+    "https://github.com/nishamusicbot1/Mitsuri-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
